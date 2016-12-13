9a8a94fbd0d42756220abd105217812dc49e756f
Martyn Eggleton <martyn.eggleton@gmail.com>
2016-12-13 21:51:51 +0000

    node-wikia code

--- examples/apps/chewie/wikia_helper.js
+++ examples/apps/chewie/wikia_helper.js
@@ -7,25 +7,78 @@ function WikiaHelper(sWikia) {
 }
 
 WikiaHelper.prototype.getArticleDetails = function(sSubject) {
-  return this.wikia.getArticleDetails({titles:[sSubject]}).then(
+  return this.wikia.getArticleDetails({ids:[],titles:[sSubject]}).then(
     function(response) {
+      console.log('success - received info for ' + sSubject, response);
       
-      console.log('success - received info for ' + sSubject);
-      return response.items[0].abstract;
-    }
-  );
-};
+      for(var sID in response.items){
+        var iID = response.items[sID].id;
+        console.log("sID", sID, "iID", iID);
+        this.wikia.getArticleAsSimpleJson(iID).then(
+          function(response) {
+            //console.log('success - found ' + sID);
+            console.log("response", response)
+          });  
+        }
+      }
+    )
+  };
+
+
+
+WikiaHelper.prototype.getArticle = function(iID, iParagraphs, iStart) {
+  //cache?
+  if(iParagraphs == null){
+    iParagraphs = 1;
+  }
+  if(iStart == null){
+    iStart = 0;
+  }
+  return this.wikia.getArticleAsSimpleJson(iID).then(
+    function(response) {
+      //console.log('success - found ' + sID);
+      //console.log("getArticle response", response);
+      
+      var aMainContent = response.sections[0].content;
+      var aParagraphs = aMainContent.filter(function(oPart){
+        return oPart.type == 'paragraph';
+      });
+      var sParagraph = aParagraphs[0].text;
+      console.log(sParagraph);
+      
+      return sParagraph;
+    });  
+        
+  };
 
 WikiaHelper.prototype.getSearchList = function(sSubject) {
   return this.wikia.getSearchList({query:sSubject}).then(
     function(response) {
       console.log('success - received info for ' + sSubject);
-      return response.body;
+      console.log(response);
+      
+      return response;
     }
   );
 };
 
-FAADataHelper.prototype.formatSearchResult = function(airportStatus) {
+WikiaHelper.prototype.getLucky = function(sSubject) {
+  return this.wikia.getSearchList({query:sSubject}).then(
+    function(oResponse) {
+      console.log('success - received info for ' + sSubject);
+      console.log(oResponse);
+      
+      if(oResponse.total>0){
+        return (oResponse.items[0].id);
+      }
+      return false;
+    }
+  );
+};
+
+//getSearchList({query:???});
+
+WikiaHelper.prototype.formatSearchResult = function(airportStatus) {
   var weather = _.template('The current weather conditions are ${weather}, ${temp} and wind ${wind}.')({
     weather: airportStatus.weather.weather,
     temp: airportStatus.weather.temp,
